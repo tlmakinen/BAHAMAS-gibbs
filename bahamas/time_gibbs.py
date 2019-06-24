55a3fe58a3d9550774d8b79c6bc3b366af3925cf
Lucas Makinen <tmakinen@princeton.edu>
2019-06-24 18:26:32 +0100

    split up scratch and good sampler files

--- bahamas/time_gibbs.py
+++ bahamas/time_gibbs.py
@@ -318,3 +318,189 @@ def runGibbs(prior, posterior_object_for_sample, ndim, niters, niters_burn, outd
 
     return n_accept_cosmo / niters, n_accept_B / niters
 
+
+
+# --------------------------------------------
+#
+#  MPI-enabled Gibbs Sampling
+#
+#---------------------------------------------
+# function to walk through steps of gibbs sampler
+#def eval_Gibbs():
+    
+
+#def runGibbs_MPI(prior, posterior_object_for_sample, 
+#            ndim, niters, niters_burn, outdir, snapshot=False, datafname=None, diagnose=False):
+
+    #from mpi4py import MPI
+    #comm = MPI.COMM_WORLD
+    
+    #size = comm.Get_size()
+    #rank = comm.Get_rank()
+
+    
+
+# --------------------------------------------
+#
+#  Testing Gibbs Posterior Draws
+#
+#---------------------------------------------
+
+def runGibbsFreeze(prior, param, posterior_object_for_sample, ndim, niters, niters_burn, outdir, plot=False):
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+    # make prior cube
+    cube = gibbs_library.makePriorCube(ndim)
+
+    # prior cube for each parameter. Takes as input uniform prior cube
+    #def makePrior(cube, ndim=1, nparams=1):
+        #cube[0] = np.random.uniform(0, 1)                    # alpha
+        #cube[1] = np.random.uniform(0, 4)                    # beta
+        #cube[2] = priors.log_uniform(cube[2], 10**-5, 10**2) # Rx
+        #cube[3] = priors.log_uniform(cube[3], 10**-5, 10**2) # Rc (CHANGE FOR NON-SYMMETRIC)
+        #cube[4] = cube[4] * 1                                # sigma_res (in likelihood code)
+        #cube[5] = priors.gaussian(cube[5], 0, 1**2)          # cstar 
+        #cube[6] = priors.gaussian(cube[6], .0796, .02)       # xstar
+        #cube[7] = priors.gaussian(cube[7], -19.3, 2.)        # mstar
+        #cube[8] = cube[8] * 1                                # omegam
+        #cube[9] = cube[9] * -4                               # w   EDIT
+        #cube[10] = 0.3 + cube[10] * 0.7                      # h   EDIT
+        #return cube
+        # start off params with priors
+    
+    true_param = param
+    #true_param =  [.14, 3.2, np.exp(.560333), np.exp(-2.3171), .1, -1.738, 0.0796, -2.605, .3, 0.7, .7]
+
+    #true_param = [0.13, 2.56, 1.0, 0.1, 0.1, 0, 0, -19.3, 0.3, 0.7, 0.72]
+    param = true_param
+    
+    ndat = posterior_object_for_sample.ndat
+    # set up empty D column-stacked latent variable array
+    D = []
+    for i in range(ndat):
+        D.append(np.zeros((1,3)))
+    # empty chain for parameter vectors
+    chains = []
+    entry_list = []
+
+    param[2:8] = prior[2:8]   #  start parameter vector as the prior vector, freezing cosmo and SALT-2
+    #param[8:] = prior[8:]
+
+    #print(param)
+    n_accept_cosmo = 0.0
+    n_accept_B = 0.0
+
+    ## TEST BURN-IN ##
+        # empty chain for parameter vectors
+    chains = []
+    
+    print('initial parameter vector = ', param)
+    
+    n_accept_cosmo = 0.0
+    n_accept_B = 0.0
+
+    chain_tab = pd.DataFrame(columns=['alpha', 'beta', 'rx', 'rc', 'sigma_res', 'cstar', 'xstar', 'mstar', 'omegam', 'w', 'h'])
+    entry_list = []
+
+    # create positive semi-definite covariance matrix for proposal distributions
+    from sklearn import datasets
+    cov_proposal_cosmo = datasets.make_spd_matrix(n_dim = 3, random_state=None) * 0.8
+    # now compute proposal sigmas for SALT-2 params
+    cov_proposal_B = datasets.make_spd_matrix(n_dim = 2, random_state=None) * 0.8
+
+    # We run our initial chains to get an estimate of the covariance between cosmo_params and cov between B_params    
+    #for iter in range(niters_burn):
+        #param,n_accept_cosmo = step_one(posterior_object_for_sample, D, param, ndim, cov_proposal_cosmo, n_accept_cosmo)
+        #param,n_accept_B = step_two(posterior_object_for_sample, D, param, ndim, cov_proposal_B, n_accept_B)
+        #entry_list.append(pd.Series(param, index=chain_tab.columns))   # move on in sample space
+        #print(param)
+
+    #print('proposal estimation acceptance fraction = ', n_accept_cosmo / niters)
+    # add to chains dataframe
+    #chain_tab = chain_tab.append(entry_list, ignore_index=True)
+
+    # compute proposal sigmas for C and B:
+    #omegam = chain_tab['omegam'].values
+    #w = chain_tab['w'].values
+    #h = chain_tab['h'].values
+
+    #alpha = chain_tab['alpha'].values
+    #beta = chain_tab['beta'].values
+
+    #cov_proposal_cosmo = np.cov(np.matrix([omegam, w, h]))  # estimated proposal covariance
+    #cov_proposal_B = np.cov(np.matrix([alpha, beta]))  
+
+    ##
+
+
+
+    print('--------- STARTING ACTUAL SAMPLER CHAIN ---------- ')
+            # set up empty D column-stacked latent variable array
+    D = []
+    for i in range(ndat):
+        D.append(np.zeros((1,3)))
+
+    # empty chain for parameter vectors
+    chains = []
+
+
+    # open write file in case the PBS job gets killed part way:
+    fname = outdir + 'post_chains_in_prog.csv'
+    with open(fname, 'w') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+
+            
+        for iter in range(niters):
+            #param,n_accept_cosmo = step_one(posterior_object_for_sample, D, param, ndim, cov_proposal_cosmo, n_accept_cosmo)
+            #param,n_accept_B = step_two(posterior_object_for_sample, D, param, ndim, cov_proposal=cov_proposal_B, n_accept_B=n_accept_B)
+            D,param = step_three(posterior_object_for_sample, D, param, ndim) # update D
+            param = step_four(posterior_object_for_sample, D, param, ndim) 
+            param = step_five(posterior_object_for_sample, D, param, ndim)
+            param = step_six(posterior_object_for_sample, D, param, ndim)
+
+            if plot == True:
+
+                D_arr = []
+                for i in range(len(D[1::3])):
+                    if (i % 3 == 0):
+                        D_arr.append([D[i], D[i+1], D[i+2]])   # append in order: [c, x, m]
+            
+                mstar_latent = np.mean(D[2::3])          # get latent cstar
+                sigma_res_latent = np.std(D[2::3])              # get latent rx
+                # plot histograms
+                if iter in [0, 1, 2, 3, 4, 5, 6, 7, 8, 100, 200]:
+
+                    fig,ax = plt.subplots(1, 1, figsize = (6,6))
+
+                    ax.hist(D[2::3], bins=50, density=True)
+                    ax.set_xlabel(r'data ${m_B}$', fontsize=15)
+                    ax.set_xlabel('latent mB distribution for iter {}'.format(iter), fontsize=15)
+                    s = 'latent mstar for iter {} = '.format(iter) + '{0:8.3f}'.format(mstar_latent)
+                    s += '\n latent sigma_res for iter {} = '.format(iter) + '{0:8.3f}'.format(sigma_res_latent)
+                    s += '\n current value for mstar in chain = {0:8.3f}'.format(param[7])
+                    s +='\n '
+                    s += r'current value for sigma_res in chain = {0:8.3f}'.format(param[4])
+
+                    m_hist = np.histogram(D[2::3], bins=50, density=True)
+                    x_pos = np.median(m_hist[1])
+                    y_pos = 0.3*np.max(m_hist[0])
+                    #print(y_pos)
+                    ax.text(-18, y_pos, s)
+                    plt.savefig('latent_mB_{}.png'.format(iter))
+            
+            if iter % 100 == 0:
+                print('current parameter vector for iter {}: '.format(iter), param)
+
+            chains.append(np.asarray(param))   # move on in sample space
+
+            wr.writerow(param)    # write the parameter step as we go.
+
+
+    print('sampling complete: cosmo param acceptance fraction = ', n_accept_cosmo/niters)
+    print('sampling complete: SALT-2 param acceptance fraction = ', n_accept_B/niters)
+    fname = outdir + 'post_chains.csv'
+    with open(fname, 'w') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        for i in chains:
+            wr.writerow(i)
